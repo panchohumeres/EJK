0e1cafd075c5a57b7d674b1e9834cc57a8cfa35c
fhumeres <fhumeres@alum.mit.edu>
2019-07-26 00:44:15 -0400

    mod

--- docker_volume_restore.sh
+++ docker_volume_restore.sh
@@ -22,8 +22,10 @@ function restore_volume {
 echo "Stopping running containers"
 docker-compose -f $compose_file_path stop
 
-echo "Mounting volumes and performing restore..."
+echo "Mounting volumes and performing restore for volumes..."
 volumes=($(docker volume ls -f name=$project_name | awk '{if (NR > 1) print $2}'))
+echo "$volumes"
+echo "end of volumes list"
 for v in "${volumes[@]}"
 do
   restore_volume $v $backup_path
