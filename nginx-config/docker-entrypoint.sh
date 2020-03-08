7894ef9ba3111940148f33735e2d791b347bcb17
fhumeres <fhumeres@alum.mit.edu>
2020-03-08 03:33:13 -0300

    test envsubst nginx.comf

--- nginx-config/docker-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-set -eu
-
-#echo "${KIBANA_GUEST_AUTH}"
-envsubst '${KIBANA_GUEST_AUTH}' < /etc/nginx/conf.d/nginx.conf.template > /etc/nginx/conf.d/nginx.conf
-#echo "$@"
-
-exec "$@"
