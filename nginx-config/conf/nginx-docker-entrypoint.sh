7bcffdbec524c8065f98bc4d5ffad53c6493a9b0
fhumeres <fhumeres@alum.mit.edu>
2020-03-10 03:35:06 -0300

    ammend

--- nginx-config/conf/nginx-docker-entrypoint.sh
+++ nginx-config/conf/nginx-docker-entrypoint.sh
@@ -1,6 +1,6 @@
 set -eu
 
 #echo "${KIBANA_GUEST_AUTH}"
-envsubst '${NGINX_SERVER_IP} ${LOCALHOST} ${DOMAIN_KIBANA} ${DOMAIN_ELASTIC} ${DOMAIN_JUPYTER} ${SERVER_NAME_KIBANA} ${SERVER_NAME_ELASTIC} ${SERVER_NAME_JUPYTER} ${KIBANA_PORT} ${ELASTIC_PORT} ${JUPYTER_PORT}' < /etc/nginx/conf.d/nginx.conf.template > /etc/nginx/conf.d/nginx.conf
+envsubst '${LOCALHOST} ${DOMAIN_KIBANA} ${DOMAIN_ELASTIC} ${DOMAIN_JUPYTER} ${SERVER_NAME_KIBANA} ${SERVER_NAME_ELASTIC} ${SERVER_NAME_JUPYTER} ${KIBANA_PORT} ${ELASTIC_PORT} ${JUPYTER_PORT}' < /etc/nginx/conf.d/nginx.conf.template > /etc/nginx/conf.d/nginx.conf
 #echo "$@"
 while :; do sleep 6h && wait $${!}; nginx -s reload; done && nginx -g \"daemon off;\"
