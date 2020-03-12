ba4b19d76699aca24a419bec40e80417e1643014
fhumeres <fhumeres@alum.mit.edu>
2020-03-12 02:10:59 -0300

    ammend

--- nginx-config/conf/nginx-docker-entrypoint.sh
+++ nginx-config/conf/nginx-docker-entrypoint.sh
@@ -1,5 +1,6 @@
 set -eu
 
+apk add curl
 #echo "${KIBANA_GUEST_AUTH}"
 envsubst '${LOCALHOST} ${DOMAIN_KIBANA} ${DOMAIN_ELASTIC} ${DOMAIN_JUPYTER} ${SERVER_NAME_KIBANA} ${SERVER_NAME_ELASTIC} ${SERVER_NAME_JUPYTER} ${KIBANA_PORT} ${ELASTIC_PORT} ${JUPYTER_PORT}' < /etc/nginx/conf.d/nginx.conf.template > /etc/nginx/conf.d/nginx.conf
 #echo "$@"
