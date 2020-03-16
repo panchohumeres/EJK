7fbb9fd35e282a1148c1b9edf303bce3d6bfc70e
fhumeres <fhumeres@alum.mit.edu>
2020-03-16 02:27:16 -0300

    a

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -8,7 +8,8 @@ do
  in 
  d) domains=(${OPTARG});; 
  p) data_path=${OPTARG};;
- e) email=${OPTARG};; 
+ e) email=${OPTARG};;
+ m) staging=${OPTARG} 
  esac 
 done 
 
@@ -22,7 +23,7 @@ fi
 rsa_key_size=4096
 #data_path="../data/certbot"
 #email="francisco@gmail.com" # Adding a valid address is strongly recommended
-staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
+#staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
