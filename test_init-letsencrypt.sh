4e277de79139cc562b7da70500d31a117b877971
fhumeres <fhumeres@alum.mit.edu>
2020-03-16 05:24:18 -0300

    a

--- test_init-letsencrypt.sh
+++ test_init-letsencrypt.sh
@@ -7,7 +7,7 @@ while getopts d:p:e: option
 do 
  case "${option}" 
  in 
- d) domains=(${OPTARG});; 
+ d) doms=(${OPTARG});; 
  p) data_path=${OPTARG};;
  e) email=${OPTARG};;
  m) staging=${OPTARG} 
@@ -25,34 +25,37 @@ rsa_key_size=4096
 #data_path="../data/certbot"
 #email="francisco@gmail.com" # Adding a valid address is strongly recommended
 #staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
+export IFS=";"
 
-if [ -d "$data_path" ]; then
-  read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
-  if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
-    exit
-  fi
-fi
+for domains in $doms; do
 
-
-if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/ssl-dhparams.pem" ]; then
-  echo "### Downloading recommended TLS parameters ..."
-  mkdir -p "$data_path/conf"
-  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf > "$data_path/conf/options-ssl-nginx.conf"
-  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem > "$data_path/conf/ssl-dhparams.pem"
-  echo
-fi
-
-echo "### Creating dummy certificate for $domains ..."
-path="/etc/letsencrypt/live/$domains"
-mkdir -p "$data_path/conf/live/$domains"
-docker-compose run --rm --entrypoint "\
-  openssl req -x509 -nodes -newkey rsa:1024 -days 1\
-    -keyout '$path/privkey.pem' \
-    -out '$path/fullchain.pem' \
-    -subj '/CN=localhost'" certbot
-echo
+    if [ -d "$data_path" ]; then
+      read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
+      if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
+        exit
+      fi
+    fi
 
 
-echo "### Starting nginx ..."
-docker-compose up --force-recreate -d nginx
-echo
+    if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/ssl-dhparams.pem" ]; then
+      echo "### Downloading recommended TLS parameters ..."
+      mkdir -p "$data_path/conf"
+      curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf > "$data_path/conf/options-ssl-nginx.conf"
+      curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem > "$data_path/conf/ssl-dhparams.pem"
+      echo
+    fi
+
+    echo "### Creating dummy certificate for $domains ..."
+    path="/etc/letsencrypt/live/$domains"
+    mkdir -p "$data_path/conf/live/$domains"
+    docker-compose run --rm --entrypoint "\
+      openssl req -x509 -nodes -newkey rsa:1024 -days 1\
+        -keyout '$path/privkey.pem' \
+        -out '$path/fullchain.pem' \
+        -subj '/CN=localhost'" certbot
+    echo
+
+
+    echo "### Starting nginx ..."
+    docker-compose up --force-recreate -d nginx
+    echo
