7629a523b99c12416a0187534479108bfe54f2f3
fhumeres <fhumeres@alum.mit.edu>
2020-03-23 22:15:54 -0300

    ammend

--- nginx-config/conf/nginx-docker-entrypoint.sh
+++ nginx-config/conf/nginx-docker-entrypoint.sh
@@ -1,7 +1,9 @@
+#!/usr/bin/env sh
+#https://serverfault.com/questions/577370/how-can-i-use-environment-variables-in-nginx-conf
 set -eu
 
 
 #echo "${KIBANA_GUEST_AUTH}"
 envsubst '${LOCALHOST} ${DOMAIN_KIBANA} ${DOMAIN_ELASTIC} ${DOMAIN_JUPYTER} ${SERVER_NAME_KIBANA} ${SERVER_NAME_ELASTIC} ${SERVER_NAME_JUPYTER} ${KIBANA_PORT} ${ELASTIC_PORT} ${JUPYTER_PORT}' < /etc/nginx/conf.d/nginx.conf.template > /etc/nginx/conf.d/nginx.conf
 #echo "$@"
-while :; do sleep 6h && wait $${!}; nginx -s reload; done && nginx -g \"daemon off;\"
+exec "$@"
