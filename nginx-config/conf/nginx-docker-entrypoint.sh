8037682cceed87b98b75f5bbb7e4ba4b6e6df4a0
fhumeres <fhumeres@alum.mit.edu>
2020-03-25 02:00:47 -0300

    AMMEND

--- nginx-config/conf/nginx-docker-entrypoint.sh
+++ nginx-config/conf/nginx-docker-entrypoint.sh
@@ -3,7 +3,7 @@
 set -eu
 
 
-export LOCALHOST=$(ifconfig | grep -E "([0-9]{1,3}\.){3}[0-9]{1,3}" | grep -v 127.0.0.1 | awk '{ print $2 }' | cut -f2 -d: | head -n1)
+export LOCALHOST=$(ip route | awk '/^default via /{print $3}')
 
 envsubst '${LOCALHOST} ${DOMAIN_KIBANA} ${DOMAIN_ELASTIC} ${DOMAIN_JUPYTER} ${SERVER_NAME_KIBANA} ${SERVER_NAME_ELASTIC} ${SERVER_NAME_JUPYTER} ${KIBANA_PORT} ${ELASTIC_PORT} ${JUPYTER_PORT}' < /etc/nginx/conf.d/nginx.conf.template > /etc/nginx/conf.d/nginx.conf
 #echo "$@"
