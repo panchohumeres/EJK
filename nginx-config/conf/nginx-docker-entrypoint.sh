a7560b2c5b4d3609d4c12b7fa5559bd09c242f3f
fhumeres <fhumeres@alum.mit.edu>
2020-03-25 18:37:42 -0300

    ammend

--- nginx-config/conf/nginx-docker-entrypoint.sh
+++ nginx-config/conf/nginx-docker-entrypoint.sh
@@ -2,7 +2,7 @@
 #https://serverfault.com/questions/577370/how-can-i-use-environment-variables-in-nginx-conf
 set -eu
 
-
+#IP LOCALHOST
 export LOCALHOST=$(ip route | awk '/^default via /{print $3}')
 
 envsubst '${LOCALHOST} ${DOMAIN_KIBANA} ${DOMAIN_ELASTIC} ${DOMAIN_JUPYTER} ${SERVER_NAME_KIBANA} ${SERVER_NAME_ELASTIC} ${SERVER_NAME_JUPYTER} ${KIBANA_PORT} ${ELASTIC_PORT} ${JUPYTER_PORT}' < /etc/nginx/conf.d/nginx.conf.template > /etc/nginx/conf.d/nginx.conf
