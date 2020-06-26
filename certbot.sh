0ffff8805dbab5c0a73f6d3955a10c9b2fb536c6
fhumeres <fhumeres@alum.mit.edu>
2020-06-26 18:25:57 -0400

    refactor certbot script

---
+++ certbot.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# script para generar certificados letsencrypt (certbot)--->https://github.com/wmnnd/nginx-certbot
+#
+# TEST SCRIPT ONLY CREATES DUMMY CERTIFICATES
+
+echo "setting environment variables"
+set -a
+. ./.env
+set +a
+
+echo "                                    "
+
+
+doms=( DOMAIN_KIBANA DOMAIN_JUPYTER DOMAIN_ELASTIC )
+data_path=${CERTBOT_PATH}
+email=${email}
+
+export IFS=";"
+
+for d in "${doms[@]}"; do
+    echo ${d}
+    echo ${email}
+    echo ${data_path}
+    ./init-letsencrypt_params_TEST.sh -d $d -p $data_path -e $email
+done
