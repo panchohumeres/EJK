676e6e0bd86ec5504216249004afc8303068d45f
fhumeres <fhumeres@alum.mit.edu>
2020-06-29 02:33:13 -0400

    ammend certbot

--- certbot.sh
+++ certbot.sh
@@ -80,12 +80,6 @@ echo "### Deleting dummy certificate for $d ..."
 done
 
 
-echo "### Requesting Let's Encrypt certificate for $domains ..."
-#Join $domains to -d args
-domain_args=""
-for domain in "${domains[@]}"; do
-  domain_args="$domain_args -d $domain"
-done
 
 # Select appropriate email arg
 case "$email" in
@@ -96,15 +90,19 @@ esac
  #Enable staging mode if needed
 if [ $staging != "0" ]; then staging_arg="--staging"; fi
 
+for d in "${domains[@]}"
+do
+echo "### Requesting Let's Encrypt certificate for $d ..."
 docker-compose run --rm --entrypoint "\
   certbot certonly --webroot -w /var/www/certbot \
-    $staging_arg \
+    $d \
     $email_arg \
     $domain_args \
     --rsa-key-size $rsa_key_size \
     --agree-tos \
     --force-renewal" certbot
 echo
+done
 
 echo "### Reloading nginx ..."
 docker-compose exec nginx nginx -s reload
