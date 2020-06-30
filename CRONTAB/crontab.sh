5f601d2c54e85619d978049694beff527bced1d6
fhumeres <fhumeres@alum.mit.edu>
2020-06-30 18:13:55 -0400

    ammend

--- CRONTAB/crontab.sh
+++ CRONTAB/crontab.sh
@@ -1,2 +1,3 @@
-* * * * * /bin/date > /tmp/date-out
-#* * * * * /bin/ls > /tmp/ls-out
+#SHELL=/bin/bash
+#BASH_ENV=/home/<user>/.bashrc_conda
+#* * * * * cd /home/<user>/PATH/TO/data-dictionary && conda activate conda-3-forge && ./init_data_dict.sh -m create && conda deactivate  > ./outputs/logs.txt 2>&1
