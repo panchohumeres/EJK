32bc1faabf67b54bc381fdd97d37c316793dd844
Ubuntu <ubuntu@bifhumeres.drftd3ganffudbd5jbegn3ygyb.nx.internal.cloudapp.net>
2020-07-01 04:23:52 +0000

    cambiando crontab

--- CRONTAB/crontab.sh
+++ CRONTAB/crontab.sh
@@ -1,1 +1,1 @@
-*/5 * * * * papermill /home/jovyan/ /ETL/dashboards_gob/CKAN_to_elastic_encrypted.ipynb /home/jovyan/ /CRONTAB/logs/CKAN_to_elastic_encrypted_`date +\%Y\%m\%d\%H\%M\%S`.ipynb
+05 * * * * papermill /home/jovyan/ /ETL/dashboards_gob/CKAN_to_elastic_encrypted.ipynb /home/jovyan/ /CRONTAB/logs/CKAN_to_elastic_encrypted_`date +\%Y\%m\%d\%H\%M\%S`.ipynb
