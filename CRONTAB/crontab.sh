413ed5c69c492fe291c8e6b7f102c035da64bb70
fhumeres <fhumeres@alum.mit.edu>
2020-06-30 23:41:53 -0400

    ammend

--- CRONTAB/crontab.sh
+++ CRONTAB/crontab.sh
@@ -1,4 +1,1 @@
-#SHELL=/bin/bash
-#BASH_ENV=/home/<user>/.bashrc_conda
-#* * * * * cd /home/<user>/PATH/TO/data-dictionary && conda activate conda-3-forge && ./init_data_dict.sh -m create && conda deactivate  > ./outputs/logs.txt 2>&1
-07 * * * * papermill /home/jovyan/ /ETL/dashboards_gob/CKAN_to_elastic_encrypted.ipynb /home/jovyan/ /CRONTAB/logs/CKAN_to_elastic_encrypted_`date +\%Y\%m\%d\%H\%M\%S`.ipynb
+5 * * * * papermill /home/jovyan/ /ETL/dashboards_gob/CKAN_to_elastic_encrypted.ipynb /home/jovyan/ /CRONTAB/logs/CKAN_to_elastic_encrypted_`date +\%Y\%m\%d\%H\%M\%S`.ipynb
